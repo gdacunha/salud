975cd512c3e1baca026329c039b811a38e7c159f
Gabriel <gdacunha05@gmail.com>
2025-09-26 16:29:06 -0400

    Created dart file for Open Food Facts API call. Added the necessary libraries for API calls as well as the products class that will be populated with data from API calls.

---
+++ salud/lib/services/openfoodfacts_service.dart
@@ -0,0 +1,7 @@
+// This file will handle the api calls for the open food facts api
+
+// api base url: 'https://world.openfoodfacts.org/api/v0'
+
+import 'dart:convert';
+import 'package:http/http.dart' as http;
+import '../models/product.dart';
