6fbe8736ea3b881693c864845515a4aa492cf398
Gabriel <gdacunha05@gmail.com>
2025-09-30 17:02:37 -0400

    Implemented basic product class with functions to convert to and from json

--- salud/lib/models/product.dart
+++ salud/lib/models/product.dart
@@ -1,1 +1,41 @@
-// this will be a food object's class which will store the food data from the open food facts api calls
+class Product {
+  final String? barcode;
+  final String? name;
+  final String? brand;
+  final String? imageUrl;
+  final Map<String, dynamic>? nutriments;
+  final String? ingredients;
+
+  Product({
+    this.barcode,
+    this.name,
+    this.brand,
+    this.imageUrl,
+    this.nutriments,
+    this.ingredients,
+  });
+
+  // Convert from API json to product object
+  factory Product.fromJson(Map<String, dynamic> json) {
+    return Product(
+      barcode: json['code'],
+      name: json['product_name'] ?? json['product_name_en'],
+      brand: json['brands'],
+      imageUrl: json['image_url'],
+      nutriments: json['nutriments'],
+      ingredients: json['ingredients_text'] ?? json['ingredients_text_en']
+    );
+  }
+
+  // Convert from Product object to json for local storage
+  Map<String, dynamic> toJson() {
+    return {
+      'code': barcode,
+      'product_name': name,
+      'brands': brand,
+      'image_url': imageUrl,
+      'nutriments': nutriments,
+      'ingredients_text': ingredients,
+    };
+  }
+}
