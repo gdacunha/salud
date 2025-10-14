89599422b920b89b81e13976cb90c8715214806d
Gabriel <gdacunha05@gmail.com>
2025-10-14 16:27:55 -0400

    Changed sdk versions to integrate the new OFF library to work

--- salud/android/app/build.gradle.kts
+++ salud/android/app/build.gradle.kts
@@ -7,7 +7,7 @@ plugins {
 
 android {
     namespace = "com.example.salud"
-    compileSdk = flutter.compileSdkVersion
+    compileSdk = 36
     ndkVersion = "29.0.14033849"
 
     compileOptions {
@@ -24,7 +24,7 @@ android {
         applicationId = "com.example.salud"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
-        minSdk = flutter.minSdkVersion
+        minSdk = 23
         targetSdk = flutter.targetSdkVersion
         versionCode = flutter.versionCode
         versionName = flutter.versionName
